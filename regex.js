c3644eefb644b954b70fb595ba44c894ae6e1363
Neeraj Rathore <neeraj8x@gmail.com>
2024-07-01 13:07:08 +0530

    Update regex.js
    added some rules from existing notes in pages by Abhishek verma sir

--- regex.js
+++ regex.js
@@ -1,3 +1,5 @@
+//https://www.debuggex.com/
+
 // let testStr = "hello"
 let testStr = "382849"
 var words = ['hello', 'hi', 'howdy'];
@@ -22,3 +24,40 @@ let data = ['a', 'b', 'c'].reduce(function (first, second, third, forouth) {
 }, 0);
 
 // console.log(data);
+ 
+let log = console.log
+let str = "ninjaa"
+// finds for ninja in string and return true 
+let regex = /[ng]inja/
+log(regex.test(str)) 
+
+let regex2 = /[abc123]000/
+log(regex2.test("a000"))  //b000, c000, 1000, 2000, 3000 works  
+
+let regex3 = /[^p]000/
+log(regex3.test("v000")) // all a000, z000 except p000
+
+let regex4 = /[^pez]000/
+log(regex4.test("a000")) // all a000, bbb000, abc000 except pez000 ppp000, p000 in any order p, e, z not allowed
+
+let regex5 = /[a-z]000/
+log(regex5.test("acsdssdsdssss000")) //  s000 match
+
+let regex6 = /[a-z][0-9]000/
+log(regex6.test("acsdssdsdssss9000")) //  s9000 match
+
+// we handle character single by single in regex by multiplying our condition below ->
+
+let regex7 = /[a-z][0-9]+000/     // + matches this at least one time to unlimited times
+log(regex7.test("acsdssdsdssss9999000")) //  s9999000 match 
+
+let regex8 = /[a-z][0-9]+000{1,}/     // + matches this at least one time to unlimited times
+log(regex8.test("acsdssdsdssss9999000a1000")) //  s9999000 match  also matched a2004000  0-9 kitne bhi time and 000 at least one time
+
+let regex9 = /[a-z][0-9]+001{3}/     
+log(regex9.test("a100111")) //  last char 3 times matches
+
+// /d = [0-9]
+// /w = [a-z, A-Z, 0-9, 's]
+// /s = space and tab
+// /t = matches tab only
